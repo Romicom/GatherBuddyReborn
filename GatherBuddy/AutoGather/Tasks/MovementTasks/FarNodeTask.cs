b661c03730520cd747508eb24606a847a8b087bb
NostraThomas99 <34015422+NostraThomas99@users.noreply.github.com>
2024-07-26 21:27:17 -0500

    Base impl

---
+++ GatherBuddy/AutoGather/Tasks/MovementTasks/FarNodeTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ECommons.Automation.NeoTaskManager;
+
+namespace GatherBuddy.AutoGather.Tasks.MovementTasks;
+
+public class FarNodeTask() : TaskManagerTask(DefaultFunction, DefaultConfiguration)
+{
+    private static Func<bool?> DefaultFunction = () =>
+    {
+        // Implement task logic here
+        return true;
+    };
+
+    // This is default configuration for FarNodeTask
+    // Needed task configuration must be set here
+    private static TaskManagerConfiguration DefaultConfiguration = new TaskManagerConfiguration()
+    {
+        TimeLimitMS    = 1000,
+        AbortOnTimeout = true,
+        // set other properties if needed
+    };
+}
